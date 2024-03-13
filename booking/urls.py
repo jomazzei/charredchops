6043891638aed8340fddd6870b858ac58a97d018
jomazzei <j.mazzei.pv@gmail.com>
2024-03-13 16:45:20 +0000

    Set up baseline views for list view and detailed view. Restricted access to detail view url to only owner of the booking

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("booknow/", views.booking, name="bookingpage"),
+    path("mybookings/", views.manage_booking_cust, name="custlist"),
+    
+    path("mybookings/<slug:slug>/", views.booking_details, name="bookingdetails"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from allauth.account.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from .models import Reservation
 
 
 # Create your views here.
@@ -7,3 +10,31 @@ def booking(request):
     Renders the booking page
     """
     return render(request, "booking/booking.html")
+
+# Refer to previous projects for list view.
+# This is a base url hook to get the link to work
+@login_required
+def manage_booking_cust(request):
+    """
+    Renders the list view.
+    Customer can manage their bookings here
+    """
+    return render(request, "booking/cust_list.html")
+
+
+@login_required
+def booking_details(request, slug):
+    """
+    Renders the details of a single reservation item
+    Customer can edit details here
+    """
+    queryset = Reservation.objects.all()
+    reservation = get_object_or_404(queryset, slug=slug)
+
+    if request.user == reservation.customer:
+        return render(request, "booking/booking_details.html",
+                  {
+                      "reservation": reservation
+                  })
+    else:
+        return HttpResponseForbidden
