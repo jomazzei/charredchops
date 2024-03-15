64fad4f89767ff7b7f95bb9ff05d780ab6413590
jomazzei <j.mazzei.pv@gmail.com>
2024-03-15 16:51:41 +0000

    Added form but won't validate through form

--- booking/forms.py
+++ booking/forms.py
@@ -1,3 +1,4 @@
+import datetime
 from django import forms
 from .models import Reservation
 
@@ -6,10 +7,22 @@ class BookTableForm(forms.ModelForm):
     """
     Form to book table
     """
-    class Meta:
-        model = Reservation
-        fields = ("cust_fname","cust_lname",
-                  "email","guest_count",
-                  "booking_date","booking_time",
-                  "comments",
+    class Meta:   
+      model = Reservation
+      fields = ("cust_fname","cust_lname",
+                "email","guest_count",
+                "booking_date","booking_time",
+                "comments",
                 )
+      labels = {
+            "cust_fname": "First name",
+            "cust_lname": "Last name",
+            "email": "Your email address",
+            "booking_date": "What day would you like to book?",
+            "booking_time": "What time would you like your reservation to be?",
+            "comments": "Special comments, requests, or allergies",
+        }
+      widgets = {
+          'booking_date': forms.DateInput(attrs={'type': 'date',}),
+          'booking_time': forms.TimeInput(attrs={'type': 'time',}),
+      }
---
+++ booking/tests_form.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from .forms import BookTableForm
+
+
+# Checks if is.valid() = True.
+# assertTrue checks if (argument) is true
+class TestBookingForm(TestCase):
+
+    def test_form_is_valid(self):
+
+        booking_form = BookTableForm({
+            "cust_fname": "Joe",
+            "cust_lname": "Mazzei",
+            "email": "j.mazzei.pv@gmail.com",
+            "booking_date": "04/12/2024",
+            "booking_time": '10:00:00',
+            "guest_count": 2,
+        })
+        if not booking_form.is_valid():
+            for field, errors in booking_form.errors.items():
+                print(f"Field: {field}")
+                for error in errors:
+                    print(f"Error: {error}")
+
+        self.assertTrue(booking_form.is_valid(), msg='Form is not valid')
